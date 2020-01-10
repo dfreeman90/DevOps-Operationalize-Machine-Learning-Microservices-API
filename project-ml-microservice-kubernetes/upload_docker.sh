3b8b435bc1214af93beebe2cd2a1b64691c7d21f
Dillon Freeman <dillonfreeman1@gmail.com>
2020-01-10 14:26:34 -0500

    add winpty wrapper to docker commands as required for windows git bash console avoiding known tty error on windows machines

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=dfreeman90/mlprojectapi
 
 # Step 2:  
 # Authenticate & tag
+# winpty wrapper required since local machine is Windows
 echo "Docker ID and Image: $dockerpath"
+winpty docker login --username dfreeman90
+winpty docker tag mlprojectimage $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+winpty docker push $dockerpath
